dc636257399da54862a33453ee306529d4027c18
Deasde34 <jorgeezp2008@hotmail.com>
2021-08-31 09:09:58 -0700

    z

---
+++ src/main.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+//#include "Person.hh"
+#include "User.hh"
+
+int main()
+{
+  /*Person* person{new Person(29, "Jesus", 1.80f, 100.f)};
+
+  std::cout << person->name << std::endl;
+  std::cout << person->age << std::endl;
+  std::cout << person->height << std::endl;
+  std::cout << person->weight << std::endl;*/
+
+  User* user1{new User(29, "Jesus", 1.80f, 100.f)};
+
+  std::cout << user1->GetName() << std::endl;
+  std::cout << user1->GetAge() << std::endl;
+  std::cout << user1->GetHeight() << std::endl;
+  std::cout << user1->GetWeight() << std::endl;
+
+  user1->SetAge(40);
+  std::cout << user1->GetAge() << std::endl;
+  user1->SetHeight(40);
+  std::cout << user1->GetHeight() << std::endl;
+  user1->SetWeight(1.76);
+  std::cout << user1->GetWeight() << std::endl;
+    user1->SetName("jose");
+  std::cout << user1->GetName() << std::endl;
+
+  delete user1;
+  std::cin.get();
+  return 0;
+}
